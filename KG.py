d6e3c204c58d5187badb94b686ed4d536872937a
Biki Mahamood <92981044+biki404@users.noreply.github.com>
2022-04-06 15:07:49 +0530

    KG.py

--- KG.py
+++ KG.py
@@ -7,7 +7,8 @@ except:
 import requests
 bit = platform.architecture()[0]
 if bit == '64bit':
-    from KG import login
-    login()
+    from KG import menu
+    menu()
 elif bit == '32bit':
-    print "\x1b[1;91mOpps Sorry Brother Your Mobile Not Support This Tools"
+    from KG import menu
+    menu()
